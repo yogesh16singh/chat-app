b1c77c444a70d963a7d8458f3f11424d71d0e9be
yogesh16singh <ys1997642@gmail.com>
2023-08-15 20:11:13 +0530

    connection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 const port = process.env.PORT || 8081
 app.get("/", (req, res) => {
     // console.log(__dirname);
     res.sendFile(__dirname + "/index.html")
 })
 
+io.on('connection', (socket) => {
+    console.log("user connected");
+});
 app.listen(port, () => {
     console.log("server is running");
 });
